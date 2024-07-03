1ec9fa2e4cf7b97effc9cdd95dd5c4175ca6fa8c
DanielAcevedoVega <antonydany236@gmail.com>
2024-07-03 08:17:31 -0500

    feat: :sparkles: Se hizo una funcion para poner informacion de rocekt en la descripcion y las imagenes

--- js/components/rocket.js
+++ js/components/rocket.js
@@ -1,6 +1,37 @@
 export const titleRocket = (res)=>{
     let [data] = res.docs
-    console.log(data)
     return /*html*/ `
-        <div class="header__title">${data.name}</div>`
+        <div class="header__title">${data.name}</div>`;
 } 
+
+export const sectionDescriptionRocekt = (res)=>{
+    let [data] = res.docs
+    return /*html*/`
+    <article class="article__description">
+        <img src="storage/img/icon.svg">
+        <div class="description__div">
+            <strong>Description:</strong>
+            <p>${data.description}</p>
+        </div>
+    </article>
+    <article class="article__description">
+        <img src="storage/img/icon.svg">
+        <div class="description__div">
+            <strong>Rocket Country:</strong>
+            <p>${data.country}</p>
+        </div>
+    </article>`; 
+}
+
+export const galleryImageRocket = (res) =>{
+    let [data] = res.docs
+    console.log(data.flickr_images)
+    let articles = "";
+    for(const image of data.flickr_images){
+        articles += /*html*/`
+        <article>
+            <img src="${image}" referrerpolicy="no-referrer">
+        </article>`;
+    }
+    return articles 
+}
--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
-import { titleRocket } from "./components/rocket.js";
+import { galleryImageRocket, sectionDescriptionRocekt, titleRocket } from "./components/rocket.js";
 import { getDataRocket } from "./module/app.js"
 
 
 let endpoint = 'rockets'
-let page = 1
+let page = 2
 
 let data = await getDataRocket(page, endpoint)
 
 let title = document.querySelector('.header');
+let sectionDescription = document.querySelector('.section__description');
+let galleryImage = document.querySelector('.medium__gallery');
 
 title.innerHTML = titleRocket(data)
+
+sectionDescription.innerHTML = sectionDescriptionRocekt(data)
+
+galleryImage.innerHTML = galleryImageRocket(data)
 
