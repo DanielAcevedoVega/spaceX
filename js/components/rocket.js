25367aa0bcd400ef4d4e5443fbd16b492f77c876
DanielAcevedoVega <antonydany236@gmail.com>
2024-07-03 11:06:19 -0500

    feat: :sparkles: Se hizo la funcion de los graficos  circle process

--- js/components/rocket.js
+++ js/components/rocket.js
@@ -55,3 +55,38 @@ export const galleryImageRocket = (res) =>{
     }
     return articles 
 }
+
+export const graphicsRocket = (res)=>{
+    let [data] = res.docs
+    let porcentaje1 = (data.engines.thrust_sea_level.kN / 2000) * 100
+    let porcentaje2 = (data.engines.thrust_vacuum.kN / 2000) * 100
+    return /*html */`
+    <div class="progress__bar" style="background: 
+    radial-gradient(closest-side, rgb(31, 31, 31) 79%, transparent 80% 100%),
+    conic-gradient(from 180deg, rgb(118, 189, 255) ${porcentaje1}%, rgba(255, 192, 203, 0) 0);  ">
+        <div>
+            <strong>Atmospheric acceleration</strong>
+            <p>${porcentaje1} % </p>
+            <p>${data.engines.thrust_sea_level.kN} kN</p>
+            <p>${data.engines.thrust_sea_level.lbf} Lbf</p>
+        </div>
+    </div>
+    <div class="progress__bar" style="background: 
+                radial-gradient(closest-side, rgb(31, 31, 31) 79%, transparent 80% 100%),
+                conic-gradient(from 180deg, rgb(118, 189, 255) ${porcentaje2}%, rgba(255, 192, 203, 0) 0);  ">
+                    <div>
+                        <strong>Speed in space</strong>
+                        <p>${porcentaje2} % </p>
+                        <p>${data.engines.thrust_vacuum.kN} kN</p>
+                        <p>${data.engines.thrust_vacuum.lbf} Lbf</p>
+                    </div>
+                </div>
+    <div class="progress__bar" style="background: 
+                radial-gradient(closest-side, rgb(31, 31, 31) 79%, transparent 80% 100%),
+                conic-gradient(from 180deg, rgb(118, 189, 255) ${data.success_rate_pct}%, rgba(255, 192, 203, 0) 0);  ">
+                    <div>
+                        <strong>Rate of success</strong>
+                        <p>${data.success_rate_pct} % </p>
+                    </div>
+                </div>`;
+} 
--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
-import { galleryImageRocket, sectionDescriptionRocekt, titleRocket } from "./components/rocket.js";
+import { galleryImageRocket, graphicsRocket, sectionDescriptionRocekt, titleRocket } from "./components/rocket.js";
 import { getDataRocket } from "./module/app.js"
 
 
 let endpoint = 'rockets'
 let page = 1
 
-let data = await getDataRocket(page, endpoint)
+let data = await getDataRocket(page, endpoint)  
 
 let title = document.querySelector('.header');
 let sectionDescription = document.querySelector('.section__description');
 let galleryImage = document.querySelector('.medium__gallery');
+let graphics = document.querySelector('.medium__graphics__item')
 
 title.innerHTML = titleRocket(data)
 
@@ -17,3 +18,4 @@ sectionDescription.innerHTML = sectionDescriptionRocekt(data)
 
 galleryImage.innerHTML = galleryImageRocket(data)
 
+graphics.innerHTML =  graphicsRocket(data)
